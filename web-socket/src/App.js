537e6491d151cb3a4c8cd4941c81f18870c8fb08
Nitesh Seram <niteshseram@gmail.com>
2022-01-08 17:28:52 +0530

    Update

--- web-socket/src/App.js
+++ web-socket/src/App.js
@@ -1,33 +1,67 @@
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
-import { useState } from "react";
+import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
 import Homepage from "./Forum/Homepage";
 import ChannelPage from "./Forum/ChannelPage";
+import CreateUser from "./CreateUser";
+let socket = '';
+const ENDPOINT = "http://localhost:3510";
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState();
 
+  useEffect(() => {
+    setIsLoading(true);
+    const user = JSON.parse(localStorage.getItem('websocketuser'));
+    if(user){
+      setUser(user);
+      setIsAuthenticated(true);
+    }
+    if(isAuthenticated){
+      socket = io(ENDPOINT);
+    }
+    setIsLoading(false);
+  }, [isAuthenticated])
+
+  // useEffect(()=> {
+    
+  //   console.log(socket);
+  // },[isAuthenticated])
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          exact
-          element={<Login setIsAuthenticated={setIsAuthenticated} />}
-        />
-        <Route
-          path="/forum"
-          exact
-          element={<Homepage setIsAuthenticated={setIsAuthenticated} />}
-        />
-        <Route
-          path="/forum/post"
-          exact
-          element={<ChannelPage setIsAuthenticated={setIsAuthenticated} />}
-        />
-      </Routes>
-    </Router>
+    <>
+      {isLoading ?
+        <div>Loading....</div>
+        :
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={<Login setIsAuthenticated={setIsAuthenticated} />}
+          />
+          <Route
+            path="/signup"
+            exact
+            element={<CreateUser />}
+          />
+          <Route
+            path="/forum"
+            exact
+            element={<Homepage isAuthenticated={isAuthenticated} user = {user}/>}
+          />
+          <Route
+            path="/forum/:id"
+            exact
+            element={<ChannelPage user={user} socket={socket} />}
+          />
+        </Routes>
+      </Router>
+    }
+    </>
   );
 };
 
